52c1eed0928f25eaa9e40c42acd4e65d5ebb4817
uraphaelp <uraphaelp@163.com>
2018-03-09 08:01:21 +0800

    Create findelein2-dimen_test.go

---
+++ findelein2-dimen/findelein2-dimen_test.go
@@ -0,0 +1,14 @@
+import (
+	"testing"
+	"fmt"
+)
+
+func TestFindele(t *testing.T) {
+	testcase:=[][]int{{1, 3, 8}, {2, 7, 10}, {6, 9, 15}}
+	fmt.Println(Findele(testcase, 8))
+	fmt.Println(Findele(testcase, 4))
+}
+
+//OUTPUT:
+true, 4
+false, 5
